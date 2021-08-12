d658091aafc1a370310aae494465e2a902fe0aa5
Sujatabatra <sujatabatra@hotmail.com>
2021-08-12 17:02:45 +0530

    Add files via upload

---
+++ Day19/SecondSpringMVCWebApp/src/com/sujata/demo/MyFirstController.java
@@ -0,0 +1,33 @@
+package com.sujata.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MyFirstController {
+
+	@RequestMapping("/first")  //MappingHandler
+	public ModelAndView firstController() {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("message", "Welcome to Spring Web MVC from /first url");
+		mv.setViewName("output");
+		return mv;
+	}
+	
+	@RequestMapping("/second")
+	public ModelAndView secondController() {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("message", "Welcome to Spring Web MVC from /second url");
+		mv.setViewName("output");
+		return mv;
+	}
+	
+	@RequestMapping("/my")
+	public ModelAndView thirdController() {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("message", "Welcome to Spring Web MVC from /my url");
+		mv.setViewName("output");
+		return mv;
+	}
+}
