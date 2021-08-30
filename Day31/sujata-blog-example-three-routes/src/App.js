5f7168370ae9921d1195955f056c1013f7636099
Sujatabatra <sujatabatra@hotmail.com>
2021-08-30 12:39:05 +0530

    Add files via upload

---
+++ Day31/sujata-blog-example-three-routes/src/App.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Create from './Create';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+          {/* <Route path="/">
+            <Home />
+          </Route> */}
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Day31/sujata-blog-example-three-routes/src/BlogList.js
@@ -0,0 +1,22 @@
+import {Link} from 'react-router-dom';
+
+  const BlogList = (props) => {
+  const blogs = props.blogs;
+  const title = props.title;
+  
+  return (
+    <div className="blog-list">
+      <h2>{ title }</h2>
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <Link to="/create">
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
+export default BlogList;
